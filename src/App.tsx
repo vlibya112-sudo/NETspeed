f61d0d2a0d1a4cc6d7cb9004098004d544b5a371
vlibya112-sudo <vlibya112@gmail.com>
2026-08-29 17:02:40 +0200

    Remove space after React import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React,{ useState, useEffect, useRef } from 'react';
 import confetti from 'canvas-confetti';
 import { 
   Gauge, 
